047600442152f8b9530f0979a50946f89690226d
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-15 07:33:07 -0300

    exception treatment during open file

--- aula11-Tratamento-de-erros.py
+++ aula11-Tratamento-de-erros.py
@@ -1,5 +1,16 @@
-try :
-    a = 1200 /0
+import time
 
-except Exception as erro:
-    print('Aconteceu algum erro:', erro)
+def abre_arquivo():
+    try :
+        open('arquivodoid.txt')
+        return True
+    except Exception as erro:
+        print('Aconteceu algum erro:', erro)
+        return False
+
+
+while not abre_arquivo():
+    print('tentando abrir o arquivo')
+    time.sleep(5)
+
+print('consegui abrir o aquivo')
