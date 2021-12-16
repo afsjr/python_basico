33a987a2e40fa8798269a11e465f6d2360b763d4
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-16 09:46:03 -0300

    API search movies omdb

--- 13-exercicio-api-filme.py
+++ 13-exercicio-api-filme.py
@@ -1,11 +1,50 @@
 import requests
 import json
-
-def requisicao(titulo):
+'''
+def existefilmes(titulo):
+    quant = 0
     try:
         req = requests.get('https://www.omdbapi.com/?apikey=25ddb129&s='+ titulo +'&type=movie')
-        print(req.text)
+        resposta = json.loads(req.text)
+
     except:
         print('falha na conexão !!!!')
+        return quant
 
-requisicao('interstellar')
+    if resposta['Response'] == 'True':
+        quant = resposta['totalResults']
+        return quant
+'''
+
+def listafilmes(titulo):
+    lista = []
+    for i in range(1,100):
+        print(i)
+        try:
+            req = requests.get('https://www.omdbapi.com/?apikey=25ddb129&s=' + titulo + '&type=movie&page='+str(i))
+            resposta = json.loads(req.text)
+            if resposta['Response'] == 'True':
+                for filme in resposta['Search']:
+                    tit = filme['Title']
+                    ano = filme['Year']
+                    string = tit + ' (' + ano + ' )'
+                    lista.append(string)
+            else:
+                print('fim das páginas')
+                break
+        except:
+            print('falha na conexão !!!!')
+    return lista
+
+sair = False
+while not sair:
+    filme = input('Digite o nome do filme para pesquisa ou SAIR : ')
+    if filme == 'SAIR':
+        sair == True
+    else:
+       filmes_encontrados= listafilmes(filme)
+       print('filmes encontrados', len(filmes_encontrados))
+       for filme in filmes_encontrados:
+           print(filme)
+
+    print('Total de filmes encontrados = ', len(filmes_encontrados))
