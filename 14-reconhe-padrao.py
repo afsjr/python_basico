fb048233d7e237e91c5677b56cbe4f2b721f607e
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-16 10:07:05 -0300

    knowing new applications for *

--- 14-reconhe-padrao.py
+++ 14-reconhe-padrao.py
@@ -2,7 +2,7 @@ import re
 
 string_de_teste = 'o gato, a gata, os gatos, as gatinhas e os gatoes'
 
-padrao = re.findall(r'gat\w',string_de_teste)
+padrao = re.findall(r'gat\w*',string_de_teste)
 
 if padrao:
     print(padrao)
