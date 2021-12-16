0ceab2a35e9d44e31a39c569f0abd597a3880621
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-16 14:07:40 -0300

    first commit quotation US dolar and EURO updated

---
+++ 15 cotacao-dolar.py
@@ -0,0 +1,13 @@
+import re
+import requests
+import json
+try:
+    requis = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL')
+except:
+    print('Não temos cotação disponível')
+
+padrao = json.loads(requis.text)
+
+print('cotação atualizada em -> ', padrao['EURBRL']['create_date'])
+print(padrao['USDBRL']['name'],' = ', padrao['USDBRL']['high'])
+print(padrao['EURBRL']['name'],' = ', padrao['EURBRL']['high'])
