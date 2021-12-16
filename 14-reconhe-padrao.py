d7a4694c5f06e3130aecf5f9472a420a574a9d65
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-16 13:32:42 -0300

    cleaning pattern

--- 14-reconhe-padrao.py
+++ 14-reconhe-padrao.py
@@ -1,14 +1,11 @@
 import re
 import requests
-lista = []
+
 requisicao = requests.get('https://www.ghenos.net/es/contactos/')
 
 padrao = re.findall(r'[\w-]+@[\w-]+\.[\w-]+', requisicao.text)
 
-lista.append(padrao)
-
 if padrao:
-    print(padrao)
     for i in padrao:
         print(i)
 else:
