14e5f3268f5caafb017ad9b34c0eba08aaf7c69a
ADELINO SANTOS <afsjr.bh@gmail.com>
2021-12-17 12:02:03 -0300

    first commit search by city w/ weather and temperature

---
+++ 15-clima-tempo.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+cidade = input('Qual cidade deseja saber o clima ? : ')
+try:
+    requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ cidade+ '&appid=6ad3139525cc940cc7c4849517bbe066')
+    dado_clima = json.loads(requisicao.text)
+
+    print('')
+    print('==========Tempo agora pelo Mundo ==============')
+    print('Cidade: ', dado_clima['name'])
+    print('Condição do tempo -> ', dado_clima['weather'][0]['main'])
+    print('Temperatura em ', dado_clima['name'], 'é ', int(dado_clima['main']['temp']) - 273, 'ºC')
+except Exception as erro:
+    print('informação não disponível no momento \nTente mais tarde')
+
+
