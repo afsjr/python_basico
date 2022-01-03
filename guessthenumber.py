47e14aaa4c51342b87f90222da50946a26ea64c4
ADELINO SANTOS <afsjr.bh@gmail.com>
2022-01-03 20:17:35 -0300

    translation and small changes

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,14 +1,17 @@
 import random
 
-i = num2 = num1 = 0
+i = num1 = 0
 num2 = random.randrange(1, 100)
-while True:
-    num1 = int(input('digite um número de 1 - 100: '))
-    i += 1
-    if num1 > num2:
-        print('ainda não encontrei, tente um valor menor diferente de', num1)
-    elif num1 < num2:
-        print('ainda não encontrei, tente um valor maior diferente de', num1)
-    else:
-        print('parabéns, você acertou depois de', i, 'tentativas')
-        False
+try:
+    while num1 != num2:
+        num1 = int(input('CHOOSE A NUMBER 1 - 100: '))
+        i += 1
+        if num1 > num2:
+            print('Not yet, the secret numer is LOWER ', num1)
+        elif num1 < num2:
+            print('Not yet, the secret numer is BIGGER ', num1)
+        else:
+            print('CONGRATS YOU GOT AFTER ', i, 'TRIES')
+
+except Exception as ValueError:
+    print('Sorry but you MUST put a integer number, try again ! ')
